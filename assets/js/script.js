cbffbf9d79291c29b54d4c158221bbaa1fc8210e
Sergey Pugin <sergey.pugin0801@yandex.ru>
2026-09-13 14:47:40 +0300

    Format Yandex Metrika script code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 // 0. Инициализация Яндекс.Метрики
 (function (m, e, t, r, i, k, a) {
-    m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-    m[i].l=1*new Date();
-    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-})(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=111850310', 'ym');
+  m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments) };
+  m[i].l = 1 * new Date();
+  for (var j = 0; j < document.scripts.length; j++) { if (document.scripts[j].src === r) { return; } }
+  k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+})(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js?id=111850310', 'ym');
 
-ym(111850310, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+ym(111850310, 'init', { ssr: true, webvisor: true, clickmap: true, ecommerce: "dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce: true, trackLinks: true });
 
 document.addEventListener('DOMContentLoaded', () => {
 
